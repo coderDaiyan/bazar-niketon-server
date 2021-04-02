e2923b4d4bc8bbaa958f6cda5a9000e3c08d5d3d
coderDaiyan <daiyanabdallah@gmail.com>
2021-04-02 13:40:29 +0600

    port changed and nodemon fix

--- index.js
+++ index.js
@@ -75,6 +75,6 @@ app.get("/", (req, res) => {
   res.send("Look Bruh! I'm Using MONGODB");
 });
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
